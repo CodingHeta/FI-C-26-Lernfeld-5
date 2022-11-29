0d26abddd638b69f82197d4b7a3af69015fde85a
CodingHeta <iz-forza@outlook.com>
2022-11-29 15:08:52 +0100

    new

--- Fahrkartenautomaten/src/ScannerDemo.java
+++ Fahrkartenautomaten/src/ScannerDemo.java
@@ -1,3 +1,37 @@
+import java.util.Scanner;// Um die Scanner-Klasse zu verwenden, muss diese importiert werden
+
 public class ScannerDemo {
-    
+
+    public static void main(String[] args) {
+
+        int ganzzahl=0;
+        double kommazahl=0.0;
+        String wort="";
+        boolean wahrheitswert=false;
+        char buchstabe=' ';
+
+        Scanner uchikomi = new Scanner(System.in); // Erzeugen eines Objekts der Klasse Scanner
+
+        System.out.print("Bitte geben Sie eine ganze Zahl ein: ");
+        ganzzahl = uchikomi.nextInt();
+        System.out.print("Sie haben " + ganzzahl + " eingegeben!\n\n");
+
+        System.out.print("Bitte geben Sie eine Zahl mit Nachkommastellen ein: ");
+        kommazahl = uchikomi.nextDouble();
+        System.out.print("Sie haben " + kommazahl + " eingegeben!\n\n");
+
+        System.out.print("Bitte geben Sie eine Wort ein : ");
+        wort = uchikomi.next();
+        System.out.print("Sie haben '" + wort + "' eingegeben!\n\n");
+
+        System.out.print("Bitte geben Sie einen Wahrheitswert ein (true / false): ");
+        wahrheitswert = uchikomi.nextBoolean();
+        System.out.print("Sie haben '" + wahrheitswert + "' eingegeben!\n\n");
+
+        System.out.print("Bitte geben Sie einen einzelnen Buchstaben ein: ");
+        buchstabe = uchikomi.next().charAt(0);
+        System.out.print("Sie haben '" + buchstabe + "' eingegeben!\n");
+       
+        uchikomi.close();
+    }
 }
