ad5c38629f55ffc7765acdc2988adfd217571e31
CodingHeta <iz-forza@outlook.com>
2023-02-22 17:19:30 +0100

    Rechtschreibung

--- Fahrkartenautomaten/src/Fahrkartenautomat.java
+++ Fahrkartenautomaten/src/Fahrkartenautomat.java
@@ -5,12 +5,18 @@ public class Fahrkartenautomat {
         // -------- HAUPTPROGRAMM !!
         Scanner uchikomi = new Scanner(System.in);
         double zuZahlen = 0;
+        double a;
+        double b;
+        double c;
 
         // hier werden die METHODEN Aufgerufen
         gruss();
         zuZahlen = bestellungErfassen(uchikomi);
-        karteBezahlen(uchikomi, zuZahlen);
-
+        a = geldEinzahlung(uchikomi, zuZahlen);
+        b = rueckgeld(uchikomi, a);
+        // c = muenzRueckgabe(b);
+        Bezahlung(uchikomi, b);
+        kartenausgabe();
         uchikomi.close();
     }
 
@@ -70,15 +76,98 @@ public class Fahrkartenautomat {
         return z;
     }
 
-    public static double karteBezahlen(Scanner uchikomi, double c) {
+    public static double geldEinzahlung(Scanner uchikomi, double c) {
         double eingezahlt = 0;
         double differenz = 0;
+        double vorzeichenwechsel = -1;
         
         System.out.print("\nSie können den Betrag jetzt Bezahlen.\n");
         eingezahlt = uchikomi.nextDouble();
         differenz = eingezahlt - c;
-        System.out.printf("\nSie haben einen Betrag i.H.v. %.2f eingegeben. Die Differenz beträgt %.2f.\n", eingezahlt, differenz);
-
+        System.out.printf("\nSie haben einen Betrag i.H.v. %.2f EUR eingegeben. Die Differenz beträgt %.2f.\n", eingezahlt, differenz);
         return differenz;
+    }
+
+    public static double rueckgeld(Scanner uchikomi, double differenz){
+      double vorzeichenwechsel = -1;
+      while(differenz < 0){
+        System.out.printf("\nBitte Zahlen Sie noch %.2f EUR ein.\n", vorzeichenwechsel*differenz);
+        differenz = differenz + uchikomi.nextDouble();
+        if(differenz > 0.009) {
+          System.out.printf("\nIhr Rückgeld beträgt %.2f.\n", differenz);
+        }
+      }
+      return differenz;
+    }
+
+    // insert method here
+
+    // aufgabe 6.3, vanilla code
+    /*
+    public static double muenzRueckgabe(double e) {
+      while (e >= 2.0) { // 2-Euro-Münzen
+        System.out.println("2 Euro");
+        e = e - 2.0;
+      }
+      while (e >= 1.0) { // 1-Euro-Münzen
+        System.out.println("1 Euro");
+        e = e - 1.0;
+      }
+      while (e >= 0.5) { // 50-Cent-Münzen
+        System.out.println("50 Cent");
+        e = e - 0.5;
+      }
+      while (e >= 0.2) { // 20-Cent-Münzen
+        System.out.println("20 Cent");
+        e = e - 0.2;
+      }
+      while (e >= 0.1) { // 10-Cent-Münzen
+        System.out.println("10 Cent");
+        e = e - 0.1;
+      }
+      while (e >= 0.05) { // 5-Cent-Münzen
+        System.out.println("5 Cent");
+        e = e - 0.05;
+      }
+    return e;
+    }
+    */
+
+    public static double Bezahlung(Scanner uchikomi, double zuZahlenderBetrag) {
+      
+      // zu gebrauchende Variablen in der Methode
+      double nochZuZahlen;
+      double eingezahlterGesamtbetrag;
+      double eingeworfeneMuenze;
+      
+      // Geldeinwurf
+      eingezahlterGesamtbetrag = 0.0;
+      nochZuZahlen = 0.0;
+      while (eingezahlterGesamtbetrag < zuZahlenderBetrag)
+      // solange eingezahlterGesamtbetrag kleiner als zuZahlenderBetrag ist
+      // wird die Schliefe weitergehen
+      {
+        nochZuZahlen = zuZahlenderBetrag - eingezahlterGesamtbetrag;
+        // NOCH zu zahlen ist differenz aus zu zahlen und (bereits) eingezahlt
+        // nZZ kommt aus der vorherigen Methode
+        System.out.printf("Noch zu zahlen: %.2f Euro \n", nochZuZahlen);
+        System.out.print("Eingabe (mind. 5 Cent, höchstens 20 Euro-Schein): ");
+        
+        eingeworfeneMuenze = uchikomi.nextDouble();
+        
+        if(eingeworfeneMuenze == 0.05 || eingeworfeneMuenze == 0.10 || eingeworfeneMuenze == 0.20 || eingeworfeneMuenze == 0.50 || eingeworfeneMuenze == 1 || eingeworfeneMuenze == 2 || eingeworfeneMuenze == 5 || eingeworfeneMuenze == 10 || eingeworfeneMuenze == 20) {
+        eingezahlterGesamtbetrag = eingezahlterGesamtbetrag + eingeworfeneMuenze;
+        }else{
+          System.out.println(">> Kein gueltiges Zahlungsmittel!");
+        }
+      }
+      
+      return eingezahlterGesamtbetrag;
+    }
+
+    public static void kartenausgabe() {
+      //hh
+      System.out.println("\nFahrschein wird ausgegeben");
+      System.out.println("==========================\n");
     }
 }
